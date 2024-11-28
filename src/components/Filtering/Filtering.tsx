8df17f5d00b7f9b35c5b590eed5d0713ed728f7c
dantauu <azazzzz455u@gmail.com>
2024-11-28 21:31:46 +0300

    ready filter full

--- src/components/Filtering/Filtering.tsx
+++ src/components/Filtering/Filtering.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react"
+import { FC, useEffect, useState } from "react"
 import plus from '../../assets/img/plus.svg'
 import minus from '../../assets/img/minus.svg'
 import rubleIcon from '../../assets/img/ruble.png'
@@ -8,6 +8,7 @@ import { CARDS, CardsServer } from "../../data/data"
 type FilterItemProps = {
 	text: string
 	onClick: (text: string) => void
+	onChange: (value: string) => void
 	id: string
 }
 
@@ -22,12 +23,16 @@ type FilteringProps = {
 	name: string
 }
 
-const FilterInputItem: FC<FilterItemProps> = ({ text, id }) => {
+const FilterInputItem: FC<FilterItemProps> = ({ text, id, onChange }) => {
 	return (
 		<label htmlFor={id}>
 			<div className='price-input__from'>
 				<div className='price-wrapper__from'>
-					<input className='input-from__inner' placeholder={text} type='number' id={id} />
+					<input className='input-from__inner' 
+					placeholder={text} 
+					type='number' 
+					id={id}
+					onChange={(e) => onChange(e.target.value)} />
 				</div>
 				<div className='price-ruble'>
 					<img src={rubleIcon} alt='' />
@@ -64,6 +69,25 @@ export const Filtering: FC<FilteringProps> = ({  setCardsData }) => {
 	const onClickOpenFull = () => {
 		setActivity(!activity)
 	}
+
+	//PRICE
+	const [minPrice, setMinPrice] = useState('')
+	const [maxPrice, setMaxPrice] = useState('')
+		useEffect(() => {
+			applyFilters()
+		}, [minPrice, maxPrice])
+
+	const applyFilters = () => {
+		let filteredPrice = CARDS
+		const minPriceNum = parseInt(minPrice, 10) || 0
+		const maxPriceNum = parseInt(maxPrice, 10) || Infinity
+
+		filteredPrice = filteredPrice.filter(card => {
+			const price = card.price
+			return price >= minPriceNum && price <= maxPriceNum
+		})
+		setCardsData(filteredPrice)
+	}
 	return (
 		<div className='filter-main__wrapper'>
 			<div className='container'>
@@ -71,8 +95,17 @@ export const Filtering: FC<FilteringProps> = ({  setCardsData }) => {
 				<div className='price-wrapper'>
 					<h2 className='price-wrapper__text'>Цена от и до</h2>
 					<div className='price-input__wrapper'>
-						<FilterInputItem text='От' onClick={() => {}} id={'от'} />
-						<FilterInputItem onClick={() => {}} text='До' id={'до'} />
+						<FilterInputItem
+							onChange={setMinPrice}
+							text='От'
+							onClick={() => {}}
+							id='minPrice'
+						/>
+						<FilterInputItem
+							onChange={setMaxPrice} 
+							onClick={() => {}} 
+							text='До'
+							id='maxPrice' />
 					</div>
 				</div>
 				<div className='before-filter'>
@@ -85,10 +118,12 @@ export const Filtering: FC<FilteringProps> = ({  setCardsData }) => {
 								onClick={() => {
 									changeCategorys(item.name), handleSelect(item.name)
 								}}
-								className={`after-filter ${select === item.name && 'selectedId'}`}
+								className={`after-filter ${
+									select === item.name && 'selectedId'
+								}`}
 							>
 								<div className='after-filter__text'>
-								  <p className='filter-discount__inner'>{item.name}</p>
+									<p className='filter-discount__inner'>{item.name}</p>
 								</div>
 							</div>
 						))}
@@ -97,36 +132,59 @@ export const Filtering: FC<FilteringProps> = ({  setCardsData }) => {
 				<ul className={`filter-drop ${isActive ? 'active' : ''}`}>
 					<li
 						onClick={() => {
-							changeCategorys('Детской'),
-							 handleSelect('Детской')
+							changeCategorys('Детской'), handleSelect('Детской')
 						}}
 						className={`filter-drop__item 
 							${select === 'Детской' ? 'selectedId' : ''}`}
 					>
-						<button className={`drop-item__inner ${select === 'Детской' ? 'selectedId' : ''}`}>Детской</button>
+						<button
+							className={`drop-item__inner ${
+								select === 'Детской' ? 'selectedId' : ''
+							}`}
+						>
+							Детской
+						</button>
 					</li>
 					<li
 						onClick={() => {
-							changeCategorys('Спальной'),
-							handleSelect('Спальной')
+							changeCategorys('Спальной'), handleSelect('Спальной')
 						}}
-						className={`filter-drop__item ${select === 'Спальной' ? 'selectedId' : ''}`}
+						className={`filter-drop__item ${
+							select === 'Спальной' ? 'selectedId' : ''
+						}`}
 					>
-						<button className={`drop-item__inner ${select === 'Спальной' ? 'selectedId' : ''}`}>Спальной</button>
+						<button
+							className={`drop-item__inner ${
+								select === 'Спальной' ? 'selectedId' : ''
+							}`}
+						>
+							Спальной
+						</button>
 					</li>
 				</ul>
-				<div style={{position: 'relative'}} className="">
-				<label htmlFor='plus'>
-					<div onClick={() => {onClickMenu(), onClickOpenFull()}} className={`open-full_wrapper 
-						${activity ? 'activity' : ''}`}>
-						<div className='open-full__img'>
-							<img className='open-full-img__inner' src={!activity ? plus : minus} alt='' />
+				<div style={{ position: 'relative' }} className=''>
+					<label htmlFor='plus'>
+						<div
+							onClick={() => {
+								onClickMenu(), onClickOpenFull()
+							}}
+							className={`open-full_wrapper 
+						${activity ? 'activity' : ''}`}
+						>
+							<div className='open-full__img'>
+								<img
+									className='open-full-img__inner'
+									src={!activity ? plus : minus}
+									alt=''
+								/>
+							</div>
+							<div className='open-full__text' id='plus'>
+								<p className='open-full__inner'>
+									{activity ? 'Скрыть' : 'Показать все'}
+								</p>
+							</div>
 						</div>
-						<div className='open-full__text' id='plus'>
-							<p className='open-full__inner'>{activity ? 'Скрыть' : 'Показать все'}</p>
-						</div>
-					</div>
-				</label>
+					</label>
 				</div>
 			</div>
 		</div>
