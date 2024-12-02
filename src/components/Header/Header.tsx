2dea33842541b8da9170a02ad9e34ee15282a1c6
dantauu <azazzzz455u@gmail.com>
2024-12-02 23:48:38 +0300

    start adaptive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import mainLogo from '../../assets/img/main-logo.svg'
 import profileIcon from '../../assets/img/profile.svg'
 import basketIcon from '../../assets/img/basket.svg'
+import burgerIcon from '../../assets/img/burger-menu.svg'
+// import krestIcon from '../../assets/img/cross.svg'
 import { Link } from 'react-router-dom'
 import { Search } from '../Search/Search'
 import { Theme } from '../../Theme/Theme'
 import { useSelector } from 'react-redux'
 import './Header.scss'
+import { useState } from 'react'
 
 // type HeaderProps = {
 // 	searchValue: string
@@ -14,6 +17,10 @@ import './Header.scss'
 
 export const Header = () => {
 	const { items } = useSelector((state: any) => state.card)
+	const [isActive, setIsActive] = useState<boolean>(false)
+	const activeBurger = () => {
+		setIsActive(!isActive)
+	}
 	return (
 		<>
 			<div className='container'>
@@ -27,33 +34,51 @@ export const Header = () => {
 						</div>
 					</div>
 					<Search />
-					<div className='header-right'>
-						<div className='header-right__img'>
-							<Theme />
+					<div className='header-right__wrapper'>
+						<div
+							onClick={() => setIsActive(false)}
+							className={`header-right ${isActive ? 'active-right' : ''}`}
+						>
+							<div className='header-right__img'>
+								<Theme />
+							</div>
+							<div className='header-right__welcome'>
+								<div className='header-welcome__img'>
+									<img
+										className='header-welcome__img__inner'
+										src={profileIcon}
+										alt=''
+									/>
+								</div>
+								<div className='header-welcome__btn'>
+									<p className='header-welcome-btn__inner'>Войти</p>
+								</div>
+							</div>
+							<Link to='/basket' className='header-welcome'>
+								<div className=''>
+									<img
+										className='header-welcome__img'
+										src={basketIcon}
+										alt=''
+									/>
+								</div>
+								<div className='header-welcome__count'>
+									<p>{items.length}</p>
+								</div>
+							</Link>
 						</div>
-						<div className='header-right__welcome'>
-							<div className='header-welcome__img'>
-								<img
-									className='header-welcome__img__inner'
-									src={profileIcon}
-									alt=''
-								/>
-							</div>
-							<div className='header-welcome__btn'>
-								<p className='header-welcome-btn__inner'>Войти</p>
-							</div>
-						</div>
-						<Link to='/basket' className='header-welcome'>
-							<div className=''>
-								<img className='header-welcome__img' src={basketIcon} alt='' />
-							</div>
-							<div className="header-welcome__count">
-								<p>{items.length}</p>
-							</div>
-						</Link>
+					</div>
+					<div className='burger-wrapper'>
+						<button onClick={() => activeBurger()} className='burger-btn'>
+							<img className='burger-img' src={burgerIcon} alt='' />
+						</button>
 					</div>
 				</div>
 			</div>
+			<div
+				style={{ borderBottom: '1px solid #ededed' }}
+				className='border'
+			></div>
 		</>
 	)
 }
