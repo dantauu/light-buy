bf73761f6312f4654d9dc12abb2c4b927d5dc7e7
dantauu <azazzzz455u@gmail.com>
2024-12-03 16:52:06 +0300

    check adapt

--- src/components/Filtering/Filtering.tsx
+++ src/components/Filtering/Filtering.tsx
@@ -2,8 +2,9 @@ import { FC, useEffect, useState } from "react"
 import plus from '../../assets/img/plus.svg'
 import minus from '../../assets/img/minus.svg'
 import rubleIcon from '../../assets/img/ruble.png'
-import './Filtering.scss'
 import { CARDS, CardsServer } from "../../data/data"
+import filterIcon from '../../assets/img/filter.svg'
+import './Filtering.scss'
 
 type FilterItemProps = {
 	text: string
@@ -85,94 +86,114 @@ export const Filtering: FC<FilteringProps> = ({  setCardsData }) => {
 		setCardsData(filteredPrice)
 	}
 	return (
-		<div className='filter-main__wrapper'>
-			<div className='container'>
-				<h2 className='filter-title'>Фильтрация</h2>
-				<div className='price-wrapper'>
-					<h2 className='price-wrapper__text'>Цена от и до</h2>
-					<div className='price-input__wrapper'>
-						<FilterInputItem
-							onChange={setMinPrice}
-							text='От'
-							onClick={() => {}}
-							id='minPrice'
-						/>
-						<FilterInputItem
-							onChange={setMaxPrice} 
-							onClick={() => {}} 
-							text='До'
-							id='maxPrice' />
-					</div>
+		<>
+			<div className='filter-svg'>
+				<div className='filter-svg__text'>
+					<h2 className='filter-svg-text__inner'>Фильтрация</h2>
 				</div>
-				<div className='before-filter'>
-					<h2 className='before-filter__title'>Для</h2>
-					<div className='before-filter__wrapper'>
-						{FilterItems.map((item, index) => (
-							<div
-								id={item.name}
-								key={index}
-								onClick={() => {
-								  changeCategorys(item.name), handleSelect(item.name)
-								}}
-								className={`after-filter ${
-									select === item.name && 'selectedId'}`}>
-								<div className='after-filter__text'>
-								  <p className='filter-discount__inner'>{item.name}</p>
-								</div>
-							</div>
-						))}
-					</div>
-				</div>
-				<ul className={`filter-drop ${isActive ? 'active' : ''}`}>
-					<li
-						onClick={() => {
-							changeCategorys('Детской'), handleSelect('Детской')
-						}}
-						className={`filter-drop__item 
-							${select === 'Детской' ? 'selectedId' : ''}`}
-					>
-						<button
-							className={`drop-item__inner ${
-								select === 'Детской' ? 'selectedId' : ''}`}>Детской
-						</button>
-					</li>
-					<li
-						onClick={() => {
-							changeCategorys('Спальной'), handleSelect('Спальной')
-						}}
-						className={`filter-drop__item ${
-							select === 'Спальной' ? 'selectedId' : ''}`}>
-						<button
-							className={`drop-item__inner ${
-								select === 'Спальной' ? 'selectedId' : ''}`}>Спальной
-						</button>
-					</li>
-				</ul>
-				<div style={{ position: 'relative' }} className=''>
-					<label htmlFor='plus'>
-						<div
-							onClick={() => {
-								onClickMenu(), onClickOpenFull()
-							}}
-							className={`open-full_wrapper ${activity ? 
-								'activity' : ''}`}
-						>
-							<div className='open-full__img'>
-								<img
-									className='open-full-img__inner'
-									src={!activity ? plus : minus}
-									alt=''
-								/>
-							</div>
-							<div className='open-full__text' id='plus'>
-								<p className='open-full__inner'>
-									{activity ? 'Скрыть' : 'Показать все'}
-								</p>
-							</div>
-						</div>
-					</label>
+				<div className='filter-svg__img'>
+					<img className='filter-svg-img__inner' src={filterIcon} alt='' />
 				</div>
 			</div>
-		</div>
+			<div className='filter-main__wrapper'>
+				<div className='container'>
+					<h2 className='filter-title'>Фильтрация</h2>
+					<div className='price-wrapper'>
+						<h2 className='price-wrapper__text'>Цена от и до</h2>
+						<div className='price-input__wrapper'>
+							<FilterInputItem
+								onChange={setMinPrice}
+								text='От'
+								onClick={() => {}}
+								id='minPrice'
+							/>
+							<FilterInputItem
+								onChange={setMaxPrice}
+								onClick={() => {}}
+								text='До'
+								id='maxPrice'
+							/>
+						</div>
+					</div>
+					<div className='before-filter'>
+						<h2 className='before-filter__title'>Для</h2>
+						<div className='before-filter__wrapper'>
+							{FilterItems.map((item, index) => (
+								<div
+									id={item.name}
+									key={index}
+									onClick={() => {
+										changeCategorys(item.name), handleSelect(item.name)
+									}}
+									className={`after-filter ${
+										select === item.name && 'selectedId'
+									}`}
+								>
+									<div className='after-filter__text'>
+										<p className='filter-discount__inner'>{item.name}</p>
+									</div>
+								</div>
+							))}
+						</div>
+					</div>
+					<ul className={`filter-drop ${isActive ? 'active' : ''}`}>
+						<li
+							onClick={() => {
+								changeCategorys('Детской'), handleSelect('Детской')
+							}}
+							className={`filter-drop__item 
+							${select === 'Детской' ? 'selectedId' : ''}`}
+						>
+							<button
+								className={`drop-item__inner ${
+									select === 'Детской' ? 'selectedId' : ''
+								}`}
+							>
+								Детской
+							</button>
+						</li>
+						<li
+							onClick={() => {
+								changeCategorys('Спальной'), handleSelect('Спальной')
+							}}
+							className={`filter-drop__item ${
+								select === 'Спальной' ? 'selectedId' : ''
+							}`}
+						>
+							<button
+								className={`drop-item__inner ${
+									select === 'Спальной' ? 'selectedId' : ''
+								}`}
+							>
+								Спальной
+							</button>
+						</li>
+					</ul>
+					<div style={{ position: 'relative' }} className=''>
+						<label htmlFor='plus'>
+							<div
+								onClick={() => {
+									onClickMenu(), onClickOpenFull()
+								}}
+								className={`open-full_wrapper ${activity ? 'activity' : ''}`}
+							>
+								<div className='open-full__img'>
+									<img
+										className='open-full-img__inner'
+										src={!activity ? plus : minus}
+										alt=''
+									/>
+								</div>
+								<div className='open-full__text' id='plus'>
+									<p className='open-full__inner'>
+										{activity ? 'Скрыть' : 'Показать все'}
+									</p>
+								</div>
+							</div>
+						</label>
+					</div>
+				</div>
+			</div>
+		</>
 	)
 }
