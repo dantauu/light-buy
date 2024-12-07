5c367f72fc162440685e91fb03d741779dfe97cd
dantauu <azazzzz455u@gmail.com>
2024-12-07 18:38:02 +0300

    fix adapt

--- src/components/Assortement/Assortement.tsx
+++ src/components/Assortement/Assortement.tsx
@@ -58,71 +58,75 @@ export const Assortement = () => {
 	return (
 		<div className='assort-wrapper'>
 			<div className='container'>
-			  <div className='mobile-wrapper'>
-				<nav className='assort-nav'>
-					<h2 className='assort-title'>Ассортимент</h2>
-					<ul className='assort-list'>
-						{AssortementNav.map((item, index) => (
-							<li
-								className={`assort-list__item 
+				<h2 className='assort-title'>Ассортимент</h2>
+				<div className='mobile-wrapper'>
+					<nav className='assort-nav'>
+						<ul className='assort-list'>
+							{AssortementNav.map((item, index) => (
+								<li
+									className={`assort-list__item 
 									${select === item.name ? 'select' : ''}`}
-								key={index}
-								onClick={() => {
-									handleSelect(item.name)
-									changeCategory(item.name)
-								}}
-							>
-								<button className='assort-button'>{item.name}</button>
-							</li>
-						))}
-						{More.map((item, id) => (
-							<button
-								key={id}
-								onClick={() => {
-									handleSelect(item.name);
-									toggleMenu()
-								}}
-								className={`assort-button more-mobile 
+									key={index}
+									onClick={() => {
+										handleSelect(item.name)
+										changeCategory(item.name)
+									}}
+								>
+									<button className='assort-button'>{item.name}</button>
+								</li>
+							))}
+							{More.map((item, id) => (
+								<button
+									key={id}
+									onClick={() => {
+										handleSelect(item.name)
+										toggleMenu()
+									}}
+									className={`assort-button more-mobile 
 								${select === item.name ? 'selected' : ''}`}
-							>
-								Ещё
-								<img className={`arrow ${arrowRotated ? 'rotated' : ''}`} src={arrow} alt='' />
-							</button>
-						))}
-					</ul>
-				</nav>
-
-				<div className='drop-wrapper'>
-					<div
-						ref={dropDownRef}
-						className={`drop-down ${isActive ? 'active' : ''}`}
-					>
-						<ul className='drop-list'>
-							<li className='drop-list__item'>
-								<button
-									onClick={() => {
-										changeCategory('Видеоглазок');
-										toggleMenu();
-									}}
-									className='drop-list__btn'
 								>
-									Видеоглазок
+									Ещё
+									<img
+										className={`arrow ${arrowRotated ? 'rotated' : ''}`}
+										src={arrow}
+										alt=''
+									/>
 								</button>
-							</li>
-							<li className='drop-list__item'>
-								<button
-									onClick={() => {
-										changeCategory('Камера');
-										toggleMenu();
-									}}
-									className='drop-list__btn'
-								>
-									Камера
-								</button>
-							</li>
+							))}
 						</ul>
+					</nav>
+
+					<div className='drop-wrapper'>
+						<div
+							ref={dropDownRef}
+							className={`drop-down ${isActive ? 'active' : ''}`}
+						>
+							<ul className='drop-list'>
+								<li className='drop-list__item'>
+									<button
+										onClick={() => {
+											changeCategory('Видеоглазок')
+											toggleMenu()
+										}}
+										className='drop-list__btn'
+									>
+										Видеоглазок
+									</button>
+								</li>
+								<li className='drop-list__item'>
+									<button
+										onClick={() => {
+											changeCategory('Камера')
+											toggleMenu()
+										}}
+										className='drop-list__btn'
+									>
+										Камера
+									</button>
+								</li>
+							</ul>
+						</div>
 					</div>
-				</div>
 				</div>
 			</div>
 		</div>
